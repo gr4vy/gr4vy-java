ead567de928d005afe23ebaf6162871e67cae577
Steve Nester <82510386+steve-gr4vy@users.noreply.github.com>
2023-10-30 12:10:52 +0000

    Improve timeout and exception handling (#31)
    - Add ability to set timeouts
    - Improve exception handling capabilities

--- src/main/java/com/gr4vy/sdk/Gr4vyException.java
+++ src/main/java/com/gr4vy/sdk/Gr4vyException.java
@@ -1,8 +1,12 @@
 package com.gr4vy.sdk;
 
+import com.google.gson.Gson;
+import com.gr4vy.api.model.ErrorGeneric;
+
 public class Gr4vyException extends RuntimeException {
 	
 	private static final long serialVersionUID = -5295936637128625156L;
+	private final Gson gson = new Gson();
 	
 	private String responseData;
 	private int httpCode;
@@ -15,11 +19,24 @@ public class Gr4vyException extends RuntimeException {
         this.responseData = responseData;
         this.httpCode = httpCode;
     }
+	public Gr4vyException(String errorMessage, Exception e) {
+		super(errorMessage, e);
+	}
 	
 	public String getResponseData() {
 		return this.responseData;
 	}
 	public int getHttpCode() {
 		return this.httpCode;
+	}
+	
+	public ErrorGeneric getError() {
+		if (responseData != null) {
+			return this.gson.fromJson(responseData, ErrorGeneric.class);
+		}
+		return null;
+	}
+	public boolean hasError() {
+		return responseData != null;
 	}
 }
